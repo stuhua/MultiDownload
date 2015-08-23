0d9d69be850bbbdf53e0cf0c7988d0c7d797a5f5
stuhua <qq850360626@gmail.com>
2015-08-23 19:13:46 +0800

    添加了进度框

--- app/src/main/java/com/mt178/multidownload/MainActivity.java
+++ app/src/main/java/com/mt178/multidownload/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,11 +18,18 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     public static final String path = "http://gdown.baidu.com/data/wisegame/fb3bba73b5437246/QQ_264.apk";
     public static final int threadCount = 3;
+    private ProgressBar pb_downlaod;
+    private EditText et_path;
+    private int runningThread = 3;
+    private int currentProgress = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pb_downlaod = (ProgressBar) findViewById(R.id.pb_downlaod);
+        et_path = (EditText) findViewById(R.id.et_path);
+
     }
 
     public void download(View view) {
@@ -36,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                     int code = conn.getResponseCode();
                     if (code == 200) {
                         int length = conn.getContentLength();//获取文件长度
+                        pb_downlaod.setMax(length);//设置progressbar的最大值
                         //创建一个与服务器大小相等的文件
                         RandomAccessFile raf = new RandomAccessFile("/sdcard/qq.apk", "rw");
                         raf.setLength(length);
@@ -48,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
                             if (threadId == threadCount) {
                                 endIndex = length;
                             }
+                            new DownLoadThread(path, threadId, startIndex, endIndex).start();
                         }
                     }
                 } catch (Exception e) {
@@ -83,13 +94,16 @@ public class MainActivity extends AppCompatActivity {
                     int downloadInt = Integer.parseInt(downloadLength);
                     //修改下载的真实位置
                     startIndex = downloadInt;
+
+                    fis.close();
                 }
                 URL url = new URL(path);
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("GET");
                 conn.setReadTimeout(5000);
                 conn.setRequestProperty("Range", "bytes=" + startIndex + "-" + endIndex);
-                if (conn.getResponseCode() == 206) {
+                int code = conn.getResponseCode();
+                if (code == 206) {
                     InputStream is = conn.getInputStream();
                     RandomAccessFile raf = new RandomAccessFile("/sdcard/qq.apk", "rw");
                     // 定位文件
@@ -101,18 +115,37 @@ public class MainActivity extends AppCompatActivity {
                     while ((len = is.read(buffer)) != -1) {
                         RandomAccessFile file = new RandomAccessFile("/sdcard/" + threadId + ".txt", "rw");
                         total += len;
+                        System.out.println("线程" + threadId + "下载到了" + total + startIndex);
                         //记录当前线程下载的数据位置
-                        file.write((total + startIndex + "").getBytes());
+                        file.write((total + startIndex - 1 + "").getBytes());
                         file.close();
                         raf.write(buffer, 0, len);
+
+                        synchronized (MainActivity.this) {
+                            // 获取所有线程下载的总进度
+                            currentProgress += len;
+                            // 更改界面上progressbar的进度(progressbar和progressdialog可以在子线程里修改UI)
+                            pb_downlaod.setProgress(currentProgress);
+                        }
                     }
 
                     raf.close();
                     is.close();
                 }
+
                 System.out.println("线程：" + threadId + "下载完毕了");
+                runningThread--;
+
             } catch (Exception e) {
                 e.printStackTrace();
+            } finally {
+                if (runningThread == 0) {
+                    for (int i = 1; i <= threadCount; i++) {
+                        File deleteFile = new File("/mnt/sdcard/" + i + ".txt");
+                        deleteFile.delete();
+                    }
+
+                }
             }
         }
     }
